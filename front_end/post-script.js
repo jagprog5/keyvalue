373568d9a416795538d01601fceaa051883ab769
John Giorshev <johngiorshev@gmail.com>
2023-01-03 21:00:32 +0000

    added toggle button

--- front_end/post-script.js
+++ front_end/post-script.js
@@ -1,7 +1,16 @@
 // script loaded at end of body
 
-const themeToggleBtn = document.querySelector(".theme-toggle");
+// prefetch theme toggle images. otherwise there is a noticable delay before display
+var lightPreload = new Image();
+var lightHoverPreload = new Image();
+var darkPreload = new Image();
+var darkHoverPreload = new Image();
+lightPreload.src = "assets/light-theme.png";
+lightHoverPreload.src = "assets/light-theme-hover.png";
+darkPreload.src = "assets/dark-theme.png";
+darkHoverPreload.src = "assets/dark-theme-hover.png";
 
+const themeToggleBtn = document.getElementById("theme-toggle");
 themeToggleBtn.addEventListener("click", function () {
   theme = theme == TOGGLED ? DEFAULT : TOGGLED;
   localStorage.setItem(THEME, theme);
